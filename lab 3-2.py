19dbc5620c89f07846bae87d266fa4a47e12200d
Jack McHale <p23jmchale@fairfieldprep.org>
2022-09-27 10:53:53 -0400

    labs 3-1 and 3-2

---
+++ lab 3-2.py
@@ -0,0 +1,8 @@
+# creater jmchale 9/27/22
+# code - GuessError, ActualError
+
+# int (a) - Syntax Error, NameError
+# int ('a') - Value Error, ValueError
+# 'a' + 2 - TypeError, TypeError
+# import date - ModuleNotFoundError, ModuleNotFoundError
+# print ("I'm a happy camper!) - Syntax Error, Syntax Error
